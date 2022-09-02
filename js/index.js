a604ed61164c689fbcf153fa7ff4fc03267d9e24
Zahid <zahid67635gmail.com>
2022-09-03 01:36:22 +0600

    spinner and not found added

--- js/index.js
+++ js/index.js
@@ -40,10 +40,21 @@ const displayCatagoriesDetails = (catagoriesDetails) => {
     console.log(catagoriesDetails)
     const newsContainer = document.getElementById('news-container')
     newsContainer.textContent = '';
+    const searchResult = document.getElementById('search-info');
+    if (catagoriesDetails.length == 0) {
+        searchResult.classList.remove('d-none');
+    }
+    else {
+        searchResult.classList.add('d-none');
+    }
     catagoriesDetails.forEach(detail => {
         const newsDiv = document.createElement('div');
         const trimDetails = detail.details.slice(0, 200);
-        newsDiv.innerHTML = `<div class="card mb-3 mx-3">
+        newsDiv.innerHTML = `
+            <div id="search-num" class="my-3 p-2">
+                <h3>${catagoriesDetails.length} items found for category Entertainment</h3>
+            </div>
+            <div class="card mb-3 mx-3">
         <div class="row g-0">
             <div class="col-md-4">
                 <img src="${detail.image_url}" class="img-fluid rounded-start" alt="...">
